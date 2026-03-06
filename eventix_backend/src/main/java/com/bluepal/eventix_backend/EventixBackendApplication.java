078b7704b055c8ad129948d975cafd0ef433982e
ankit-kumar <ankitkumar99au@gmail.com>
2026-03-06 21:24:23 +0530

    backend project created succes fully

---
+++ eventix_backend/src/main/java/com/bluepal/eventix_backend/EventixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bluepal.eventix_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventixBackendApplication.class, args);
+    }
+
+}
---
+++ eventix_backend/src/test/java/com/bluepal/eventix_backend/EventixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluepal.eventix_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
